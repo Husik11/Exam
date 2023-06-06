370579feabe828f39f2d0ec70c192c6aab2c723f
Husik Hovhannisyan <124252322+Husik11@users.noreply.github.com>
2023-06-06 13:56:10 +0400

    Update 42. Trapping Rain Water.py
    Change names

--- 42. Trapping Rain Water.py
+++ 42. Trapping Rain Water.py
@@ -3,15 +3,15 @@ height = [4, 2, 0, 3, 2, 5]
 def trap(height):
     if not height:
         return 0
-    water = 0
+    w_trap = 0
     n = len(height)
     for i in range(1, n - 1):
         left_max = max(height[:i])
         right_max = max(height[i + 1:])
         min_height = min(left_max, right_max)
         if min_height > height[i]:
-            water += min_height - height[i]
-    return water
+            w_trap += min_height - height[i]
+    return w_trap
 print(trap(height))
 
 
